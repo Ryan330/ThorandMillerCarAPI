1e776761f14f3fbb34bd1ce18d392a1db46cf38e
Thor Denson <tjdenson@eckerd.edu>
2018-06-04 21:32:35 -0400

    masaged API to fit whith what needs to load on page. One hell of a commit

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,6 @@
 
 const fileURL = '../index.html';
 
-   
-//data-targetMake
-//data-targetModel
-//data-targetYear
-
   //Global variable for the ID of the model car we're getting data for.
   var currentId = 1;
 
@@ -21,6 +16,8 @@ var yearOptionSelect = "[data-targetYear]";
 var yearOption = document.querySelector(yearOptionSelect);
 var modelOptionSelect = "[data-targetModel]";
 var modelOption = document.querySelector(modelOptionSelect);
+var submitButtonSelect = "[data-targetButton]";
+var submitButton = document.querySelector(submitButtonSelect);
 
 
 //Data Function (Make)
@@ -33,10 +30,8 @@ $.get(dataURL, function (data) {
     //Implement Keys in Array
     dataKeys.forEach(function (key) {
         var carInfo = data[key];
-
         dataArray.push(carInfo);
     });
-
 
 
     //Load and Implement Data in HTML
@@ -46,14 +41,17 @@ $.get(dataURL, function (data) {
         var nameMake = data.name;
         var carID = data.id;
 
+        var $modelDropdown = $("<option>", {
+          name: carID,
+          value: `${carID}`,
+          text: `${nameMake}`,
+      }); 
+         
         //Create HTML Option
-        $(nameOption).append($("<option>", {
-            value: `${carID}`,
-            text: `${nameMake}`
-        }));
-    });
-});
-
+        $modelDropdown.attr({make : nameMake});
+        $(nameOption).append($modelDropdown);
+    });//ends dataArray.forEach
+});//ends .get function
 
 
 nameOption.addEventListener("change", function () {
@@ -61,15 +59,19 @@ nameOption.addEventListener("change", function () {
   var model = $('#data-targetModel');
   var years = $('#data-targetYear');
   console.log(event.target.value);
+  //console.log(event.target.make);
+  console.log(event.target.name);
+  console.log(event.target);
+  var option = document.getElementsByName(event.target.value);
+  console.log(option[0].innerHTML);
+  localStorage.setItem("makeIDKey", option[0].innerHTML);
+
 
   const dataModelURL = "https://api.fuelapi.com/v1/json/models/"+event.target.value+"/&api_key=daefd14b-9f2b-4968-9e4d-9d4bb4af01d1";
  console.log(dataModelURL);
 
 
-//**Year dropdown**/
-
-
-
+//**Year dropdown**
   const dataYearURL = "https://api.fuelapi.com/v1/json/modelYears/52/&api_key=daefd14b-9f2b-4968-9e4d-9d4bb4af01d1";
 
   $.get(dataYearURL,
@@ -77,14 +79,14 @@ nameOption.addEventListener("change", function () {
       $(yearOption).append($("<option>", {
         value: ``,
         text: `Any Year`
-     }));
+     })); //end .append
       response.forEach(function (data) {
         $(yearOption).append($("<option>", {
           value: `${data}`,
           text: `${data}`
        }));
-     });
-  });
+     });//end .forEach
+  }); //end .get
 
   //enables the Model Dropdown once year has been selected
   $('select[data-targetYear]').on("change", function(){
@@ -103,136 +105,42 @@ nameOption.addEventListener("change", function () {
         var carModelID = data.id;
 
           $(modelOption).append($("<option>", {
-            value: `${carModelID}`,
+            value: `${modelMake}`,
             text: `${modelMake}`
           }));
-      });
-  });
+      });//end forEach
+  }); //end .get
 
   yearOption.addEventListener("change", function(){
     console.log(event.target.value);
+    localStorage.setItem("yearIDKey", event.target.value);//stores selected year to local storage
     });
-});
 
+    modelOption.addEventListener("change", function(){
+      console.log(event.target.value);
+      localStorage.setItem("modelIDKey", event.target.value);//stores selected model to local stoage
+      
 
-//  Action after submit button has been pressed
+      localStorage.setItem("makeIDKey", option[0].innerHTML);
+     
+   
 
-$('input[type=submit]').click(function() {
-  window.location.href = ""
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//***********************Setup of second page***********************************
-
-// jQuery-ified versions of our DOM elements
-const $prevButton = $('[data-control-prev]');
-const $nextButton = $('[data-control-next]');
-const $carInfo    = $('[data-car-info]');
-
-//Calculates the previous ID 
-function prevId() {
-  //Don't decrement if currentId is 1
-  if (currentId > 1) {
-      currentId = currentId - 1;
-  }
-}
-
-//Calculates the next ID
-function nextId() {
-    currentId = currentId + 1;
-}
-
-
-//Creates the container for the car's info
-function createCard(carData) {
-   var $carCard = $('div');
-   var $carInfoList = createList(carData);
-   $carCard.append($carInfoList);
-   return $carCard;
-}
-
-//Creates a list of specific car infomation
-function createList(carData) {
-    var$carInfoList = $('<ul>');
-
-
-    Object.keys(carData).forEach(function (key) {
-        var val = carData[key];
-    
-        if (Array.isArray(val)) {
-          if (val.length > 0 && val[0] !== '') {
-            console.log(val);
-            var arrayString = val.join(', ');
-            var $carInfoItem = $('<li>', {
-              text: `${key}: [${arrayString}]`
-            });
-    
-            $carInfoList.append($carInfoItem);
-          }
-        } else if (val !== '') {
-          var $carInfoItem = $('<li>', {
-            text: `${key}: ${val}`
-          });
-    
-          $carInfoList.append($carInfoItem);
-        }
-    
+      //Toggle Submit Button
+      submitButton.classList.toggle("buttonHidden");
       });
-    
-      return $carInfoList;
-    }
-    
 
-//Draws car info to the page
-function drawCar(carData) {
-    console.log(carData);
-    var $carCard = createCard(carData);
-      
-      
-   //Empty out the current car info
-   $carInfo.html('');
-      
-   //Put our new car card in the box
-   $carInfo.append($carCard);
-  
-}
+});//ends nameOption.addEventListener()
 
-//Makes Ajax req, then draws car info to page
-function getAndDrawCar() {
-    getCar()
-      .then(drawCar)
-  }
-  
-  //Attaches event listeners to buttons.
-  //Makes initial Ajax request for first car
-  function main() {
-  
-    $prevButton.on('click', function (event) {
-      event.preventDefault();
-      prevId();
-      getAndDrawCar();
-    });
-  
-    $nextButton.on('click', function (event) {
-      event.preventDefault();
-      nextId();
-      getAndDrawCar();
-    });
-  
-    getAndDrawCar();
-  }
+//Action after submit button has been pressed
+$('input[type=submit]').click(function() {
+  window.location.href = "results.html"
+});
 
-//Sets it off
-main()
+
+
+
+
+
+
+
+
---
+++ scripts/results.js
@@ -0,0 +1,189 @@
+
+//HTML Targets
+var resultsImageSelector = "[data-targetImage]";
+var resultsImage = document.querySelector(resultsImageSelector);
+
+
+//retrieve local storage car ID
+var modelIDKey = localStorage.getItem("modelIDKey");
+console.log(modelIDKey);
+
+var makeIDKey = localStorage.getItem("makeIDKey");
+console.log(makeIDKey);
+
+var yearIDKey = localStorage.getItem("yearIDKey");
+console.log(yearIDKey);
+
+
+
+
+const dataModelURL = "https://api.fuelapi.com/v1/json/vehicles/?year="+yearIDKey+"&model="+modelIDKey+"&make="+makeIDKey+"&api_key=daefd14b-9f2b-4968-9e4d-9d4bb4af01d1";
+ console.log(dataModelURL);
+
+ $.get(dataModelURL,
+    function(vehicleInfoArray) {
+        console.log(vehicleInfoArray);
+        console.log(vehicleInfoArray[0].id);
+        var vehicleID = vehicleInfoArray[0].id;
+        const mediaURL = "https://api.fuelapi.com/v1/json/vehicle/"+vehicleID+"/?api_key=daefd14b-9f2b-4968-9e4d-9d4bb4af01d1";
+        console.log(mediaURL);
+
+        $.get(mediaURL, function(dataMedia){
+            console.log(dataMedia);
+            console.log(dataMedia.products);
+
+            createCard(dataMedia);
+        
+        });
+    }
+);
+
+
+
+
+
+
+
+
+
+
+
+// Makes XMLHttpRequest()
+var request = new XMLHttpRequest();
+
+// jQuery-ified versions of our DOM elements
+const $prevButton = $('[data-control-prev]');
+const $nextButton = $('[data-control-next]');
+const $carInfo    = $('[data-car-info]');
+
+//Calculates the previous ID 
+function prevId() {
+
+  if (currentId > 1) {
+      currentId = currentId - 1;
+  }
+}
+
+function nextId() {
+    currentId = currentId + 1;
+}
+
+
+//Creates the container for the car's info
+
+  var createCard = (result) => {
+    var $carCard = $('<div>');
+    var $carInfoList = $('<ul>');
+    $carCard.append($carInfoList);
+    var $results = $('.results'); //targeting results in HTML
+    $results.append($carCard);
+  
+    var $trim = $('<li>').text(result.trim);
+    $carInfoList.append($trim);
+  
+    var $driveTrain = $('<li>').text(result.drivetrain)
+    $carInfoList.append($driveTrain);
+    
+    var $sideImage = $('<li>');
+    var sideImageUrl = result.products[0].productFormats[0].assets.find(asset => {
+      return asset.shotCode.code === '037'
+    }).url;
+    
+  console.log(sideImageUrl); //ToDo:  append the created DOM element****
+
+  };
+
+
+
+
+
+////Creates a list of specific car infomation
+//function createList(carData) {
+//    var $carInfoList = $('<ul>');
+
+
+    // Object.keys(carData).forEach(function (key) {
+    //     var val = carData[key];
+    
+    //     if (Array.isArray(val)) {
+    //       if (val.length > 0 && val[0] !== '') {
+    //         console.log(val);
+    //         var arrayString = val.join(', ');
+    //         var $carInfoItem = $('<li>', {
+    //           text: `${key}: [${arrayString}]`
+    //         });
+    
+    //         $carInfoList.append($carInfoItem);
+    //       }
+    //     } else if (val !== '') {
+    //       var $carInfoItem = $('<li>', {
+    //         text: `${key}: ${val}`
+    //       });
+    
+    //       $carInfoList.append($carInfoItem);
+    //     }
+    
+    //   });
+    
+    //   return $carInfoList;
+    // }
+    
+
+//Draws car info to the page
+function drawCar(carData) {
+    console.log(carData);
+    var $carCard = createCard(carData);
+      
+      
+   //Empty out the current car info
+   $carInfo.html('');
+      
+   //Put our new car card in the box
+   $carInfo.append($carCard);
+  
+}
+
+//Makes Ajax req, then draws car info to page
+function getAndDrawCar() {
+    getCar()
+      .then(drawCar)
+  }
+  
+  //Attaches event listeners to buttons.
+  //Makes initial Ajax request for first car
+  function main() {
+  
+    $prevButton.on('click', function (event) {
+      event.preventDefault();
+      prevId();
+      getAndDrawCar();
+    });
+  
+    $nextButton.on('click', function (event) {
+      event.preventDefault();
+      nextId();
+      getAndDrawCar();
+    });
+  
+    getAndDrawCar();
+  }
+
+//Sets it off
+//main()
+
+var imgURL = "https://api.fuelapi.com/v1/json/vehicle/19100/?api_key=daefd14b-9f2b-4968-9e4d-9d4bb4af01d1&productID=1";
+//
+//$.get(imgURL, 
+//    function (response) {
+//        response.forEach(function (data) {
+//          var imgID = data.id;
+//          
+//        });
+//
+//
+//
+//
+//
+//$(nameOption).append($("<img>", {
+//    src: `${imgID}`
+//}));
